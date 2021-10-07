931ef3ce2284e8cf43bed9c79eb54a0dc2222c9c
CamileGhastine <camileghastine@hotmail.com>
2021-10-07 17:12:04 +0200

    challenge 1 : cart correction

--- Cart/tests/CartTest.php
+++ Cart/tests/CartTest.php
@@ -3,6 +3,7 @@
 use PHPUnit\Framework\TestCase;
 use Cart\Cart;
 use Cart\Product;
+use Cart\Storable;
 
 class CartTest extends TestCase
 {
@@ -19,10 +20,10 @@ class CartTest extends TestCase
             'orange' => new Product('orange', 7.5),
         ];
         extract($this->products);
-        
-        $this->storage = new MockStorage;
-        $this->storage->setValue($orange->getName(), $orange->getPrice()*2*1.2);
-        $this->storage->setValue($strawberry->getName(), $strawberry->getPrice()*5*1.2);
+
+        $this->storage = $this->createMock(Storable::class);
+        $this->storage->setValue($orange->getName(), $orange->getPrice() * 2 * 1.2);
+        $this->storage->setValue($strawberry->getName(), $strawberry->getPrice() * 5 * 1.2);
 
         $this->cart = new Cart($this->storage);
     }
@@ -33,12 +34,10 @@ class CartTest extends TestCase
     public function testBuy()
     {
         extract($this->products);
-        $this->cart->buy($apple,3);
-        $key = array_key_last($this->storage->getStorage());
-        $value = $this->storage->getStorage()[$key];
 
-        $this->assertSame($key, 'apple');
-        $this->assertSame($value, 3*10.5*1.2);
+        $this->storage->expects($this->once())->method('setValue')->with($apple->getName(), 37.8);
+
+        $this->cart->buy($apple, 3);
     }
 
     /**
@@ -46,9 +45,9 @@ class CartTest extends TestCase
      */
     public function testReset()
     {
-        $this->cart->reset();
+        $this->storage->expects($this->once())->method('reset');
 
-        $this->assertEmpty($this->storage->getStorage());
+        $this->cart->reset();
     }
 
     /**
@@ -58,11 +57,9 @@ class CartTest extends TestCase
     {
         extract($this->products);
 
-        $this->assertContains($strawberry->getPrice()*5*1.2, $this->storage->getStorage());
+        $this->storage->expects($this->once())->method('restore')->with($strawberry->getName());
 
         $this->cart->restore($strawberry);
-        $this->assertContains($orange->getPrice()*2*1.2, $this->storage->getStorage());
-        $this->assertNotContains($strawberry->getPrice()*5*1.2, $this->storage->getStorage());
     }
 
     /**
@@ -70,7 +67,16 @@ class CartTest extends TestCase
      */
     public function testTotal()
     {
-        $this->assertSame($this->cart->total(), 45.0 + 18.0);
+        $this->storage->method('getStorage')->willReturn([
+            'apple' => 10 * 10.5 * 1.2,
+            'orange' => 5 * 7.5 * 1.2,
+        ]);
+
+        $this->storage->expects($this->once())->method('getStorage');
+
+        $sum = 10 * 10.5 * 1.2 + 5 * 7.5 * 1.2;
+
+        $this->assertEquals($this->cart->total(), round($sum, 2));
     }
 
     /**
@@ -84,7 +90,7 @@ class CartTest extends TestCase
 
         $this->assertSame(0.2, $defaultTVA);
     }
-    
+
     /**
      * @test testRestoreQuantity test if we can remove a specific quantity of one product
      */
@@ -92,13 +98,17 @@ class CartTest extends TestCase
     {
         extract($this->products);
 
-        $this->cart->restoreQuantity($strawberry, 2);
+        $this->storage->method('getStorage')->willReturn([
+            'apple' => 10 * 10.5 * 1.2,
+            'orange' => 5 * 7.5 * 1.2,
+        ]);
+        
+        $newValue = 10 * 10.5 * 1.2 - 5 * 10.5 * 1.2;
 
-        $key = $strawberry->getName();
-        $value = $this->storage->getStorage()[$key];
+        $this->storage->expects($this->once())->method('restore')->with($apple->getName());
+        $this->storage->expects($this->once())->method('setValue')->with($apple->getName(), $newValue);
 
-        $this->assertSame($key, 'strawberry');
-        $this->assertSame($value, 3*7.5*1.2);
+        $this->cart->restoreQuantity($apple, 5);
     }
 
     /**
@@ -108,9 +118,14 @@ class CartTest extends TestCase
     {
         extract($this->products);
 
-        $this->expectExceptionMessage("Pas de {$apple->getName()} dans le panier.");
+        $this->storage->method('getStorage')->willReturn([
+            'apple' => 10 * 10.5 * 1.2,
+            'orange' => 5 * 7.5 * 1.2,
+        ]);
 
-        $this->cart->restoreQuantity($apple, 3);
+        $this->expectExceptionMessage("Pas de {$strawberry->getName()} dans le panier.");
+
+        $this->cart->restoreQuantity($strawberry, 5);
     }
 
     /**
@@ -119,6 +134,12 @@ class CartTest extends TestCase
     public function testRestoreQuantityExceptionProductNotEnoughInStorage()
     {
         extract($this->products);
+
+        $this->storage->method('getStorage')->willReturn([
+            'apple' => 10 * 1.5 * 1.2,
+            'orange' => 10 * 1.2 * 1.2,
+            'bananas' => 10 * 1.3 * 1.2,
+        ]);
 
         $this->expectExceptionMessage("Il n'y pas assez de {$orange->getName()} dans votre panier pour en retirer 1000");
 
